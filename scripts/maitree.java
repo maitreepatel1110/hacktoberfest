d6a5087074ec7c27eb73cbaf3d889e07347e3a4b
maitreepatel1110 <132289240+maitreepatel1110@users.noreply.github.com>
2024-10-21 16:59:51 +0530

    Add hello world script
    Added hello world script in java

---
+++ scripts/maitree.java
@@ -0,0 +1,11 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: Maitree Patel
+  GITHUB: https://github.com/maitreepatel1110
+*/
+
+public class Maitree {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
